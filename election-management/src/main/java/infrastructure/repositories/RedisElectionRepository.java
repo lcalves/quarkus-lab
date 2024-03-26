a4569d8356b04202c9f339a7517bb1571488847b
Leonardo Campos Alves <leonardo_lca@yahoo.com.br>
2024-03-26 08:45:40 -0300

    feat: implemented items from class 10

--- election-management/src/main/java/infrastructure/repositories/RedisElectionRepository.java
+++ election-management/src/main/java/infrastructure/repositories/RedisElectionRepository.java
@@ -9,6 +9,7 @@ import io.quarkus.redis.datasource.sortedset.ScoreRange;
 import io.quarkus.redis.datasource.sortedset.SortedSetCommands;
 
 import javax.enterprise.context.ApplicationScoped;
+import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
 
@@ -33,6 +34,12 @@ public class RedisElectionRepository implements ElectionRepository {
         commands.zadd("election:" + election.id(), rank);
         pubsub.publish("elections", election.id());
     }
+
+    @Override
+    public List<Election> findAll() {
+        throw new UnsupportedOperationException();
+    }
+
 
     public Election sync(Election election) {
         var map = commands.zrangebyscoreWithScores("election:" + election.id(),
